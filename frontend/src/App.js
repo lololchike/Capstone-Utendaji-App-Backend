16baf5accbde876042e32f0a7d7ab2aba7b31bff
Bill <billotungah@gmail.com>
2023-06-13 10:34:49 +0300

    add chat button next to online users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import EmployeeHome from './pages/employeeHome';
 import Login from './pages/login';
 
 function App() {
+  
   return (
-    <EmployeeHome />
+      
+        <div className="App">
+          
+            <EmployeeHome />
+           
+        </div>
+
   );
 }
 
 export default App;
+
+/* <Router>
+<div className="App">
+  <Routes>
+    <Route path="/" element = {<Login />}/>
+    <Route path="/employee" element = {<EmployeeHome />}/>
+  </Routes>
+</div>
+</Router> */
--- frontend/src/pages/employeeHome.js
+++ frontend/src/pages/employeeHome.js
@@ -7,8 +7,8 @@ const EmployeeHome = () => {
             <div id="header"></div>
 
         <div id="sidebar">
-        <div id="avatardiv"><h3>BO</h3></div>
-        <h4 id="employeeNameonsidebar">Bill Otunga</h4>
+        <div id="avatardiv"><h3>EM</h3></div>
+        <h4 id="employeeNameonsidebar">Elisa Marie</h4>
         <strong><p>My Performance</p></strong>
         <strong><p>My Team</p></strong>
         <strong><p>My Jobs</p></strong>
@@ -20,43 +20,74 @@ const EmployeeHome = () => {
             <div id="online-employee">
             <strong><p>Hoodie Guy</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
     <div id="online-employee">
             <strong><p>Excel Wizard</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
     <div id="online-employee">
             <strong><p>Lunchtime Legend</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
     <div id="online-employee">
             <strong><p>Coffee Queen</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
     <div id="online-employee">
             <strong><p>Keyboard Ninja</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
     <div id="online-employee">
             <strong><p>Office Deejay</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
     <div id="online-employee">
             <strong><p>Email Lord</p></strong>
             <div id="greenonlinelight"></div>
+            <img className="sendmessagesvg" src="chat.png" alt="image" />
     </div>
         </div>
 
             <input type="text" id="searchbar" placeholder ="Search"/>
         <div id="middlegrid">
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
-            <div className = "KPI-grid"></div>
+            <div className = "KPI-boxes">
+                <h4>Handling Time </h4> 
+                <h2>3.27</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>Revenue </h4> 
+                <h2>600,000</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>MIA (Days) </h4> 
+                <h2>1</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>Handling Time </h4> 
+                <h2>3.27</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>Propositions </h4> 
+                <h2>50</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>Airtime </h4> 
+                <h2>70,000</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>Devices</h4> 
+                <h2>30</h2>
+                </div>
+            <div className = "KPI-boxes">
+            <h4>Total Sales</h4> 
+                <h2>2,000,000</h2>
+                </div>
         </div>
         <div id="footer"></div>
         </div>
